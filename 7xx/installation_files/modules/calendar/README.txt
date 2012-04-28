8bf9f1f0cdf70a77c43e77b25d7b34f2218cf16f
Adam Pope <adamrpope@gmail.com>
2012-04-28 17:55:09 -0400

    Updated Module Files
    Updated files to use for testing.

---
+++ 7xx/installation_files/modules/calendar/README.txt
@@ -0,0 +1,12 @@
+Creates calendar displays of Views results.
+
+Create a new calendar by using the calendar template for the desired
+date field. See a link to create a view from a template at the top
+of the Views listing page.
+
+If the Calendar iCal module is enabled, an iCal feed can be
+attached to the view.
+
+For a quick and easy start, install the Date Tools module, then use
+the Date Tools Wizard to create a content type with a date field
+and a calendar that displays that field, all in a single step!
---
+++ 7xx/installation_files/modules/maestro/theme/structure/tasks/maestro-task-if.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * maestro-task-if.tpl.php
+ */
+
+?>
+
+<div class="maestro_task">
+  <div class="t"><div class="b"><div class="r"><div class="l"><div class="bl"><div class="br"><div class="tl-yel"><div class="tr-yel">
+
+    <div id="task_title<?php print $tdid; ?>" class="tm-yel maestro_task_title">
+      <?php print $taskname; ?>
+    </div>
+    <div class="maestro_task_body">
+      <?php print t('If Condition'); ?>
+    </div>
+
+  </div></div></div></div></div></div></div></div>
+</div>
